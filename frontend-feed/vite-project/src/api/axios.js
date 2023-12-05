2221d0343b8025c55c9536aee4b56c4c2cf7be3f
Elmer-sv <00155620@uca.edu.sv>
2023-12-05 10:51:05 -0600

    fixed v2

--- frontend-feed/vite-project/src/api/axios.js
+++ frontend-feed/vite-project/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const instance = axios.create({
-    baseURL: "https://friendly-jade-smock.cyclic.app",
+    baseURL: "https://friendly-jade-smock.cyclic.app/api",
 })
 
 export default instance
